7e100bd89da26a614d7af3bd23c21673dbbfb477
Mohamed Rifays <rifaysmohamed2005@gmail.com>
2025-08-16 23:25:04 -0700

    installed hbs and set up hbs and how worked on hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+import hbs from 'hbs'
+import { title } from 'process';
 
 
 const app = express();
 app.use(cors());
 app.use(express.json())
+
+//getting the name of the file and directory
 
 const __filename = fileURLToPath(import.meta.url);
 console.log(__filename);
@@ -15,14 +18,42 @@ console.log(__filename);
 const __dirname = path.dirname(__filename);
 console.log(__dirname);
 
-
+//joining the dirname with the the public directory
 const renderHTML = path.join(__dirname,'../public')
 
 app.use(express.static(renderHTML))
+ 
+//creating the hbs and joining it with the partials
+hbs.registerPartials(path.join(__dirname,'../templates/partials'))
 
-
+//setting the view of hbs
 app.set('view engine','hbs')
 
+ app.set('views',path.join(__dirname,'../templates/views'))
+
+app.get('',(req,res)=>{
+  res.render('index',{title:'hbs setup',
+    message:'dynamic html',
+     footer:'Created by : Mohamed Rifays'
+  })
+})
+
+app.get('/help',(req,res)=>{
+
+  res.render('help',{
+    title:'help page',
+    message:'help message.',
+    footer:'Created by : Mohamed Rifays'
+  })
+})
+
+app.get('/contact',(req,res)=>{
+   res.render('contact',{
+    title:'contact page',
+    message:'contact number : 8778867174',
+     footer:'Created by : Mohamed Rifays'
+   })
+})
 
 app.post('/home',(req,res)=>{
 
